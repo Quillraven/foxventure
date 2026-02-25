d884c77056a6d4bc174d4cebd399f5c0b37f5f11
Quillraven <quillraven@gmail.com>
2026-02-25 22:21:03 +0100

    add proximity detection and follow system

--- core/src/main/kotlin/io/github/quillraven/foxventure/screen/GameScreen.kt
+++ core/src/main/kotlin/io/github/quillraven/foxventure/screen/GameScreen.kt
@@ -18,11 +18,13 @@ import io.github.quillraven.foxventure.system.ClimbSystem
 import io.github.quillraven.foxventure.system.CollisionSystem
 import io.github.quillraven.foxventure.system.ControllerSystem
 import io.github.quillraven.foxventure.system.DelayRemovalSystem
+import io.github.quillraven.foxventure.system.FollowSystem
 import io.github.quillraven.foxventure.system.FsmSystem
 import io.github.quillraven.foxventure.system.GroundMoveSystem
 import io.github.quillraven.foxventure.system.PhysicsTimer
 import io.github.quillraven.foxventure.system.PostInterpolationSystem
 import io.github.quillraven.foxventure.system.PreInterpolationSystem
+import io.github.quillraven.foxventure.system.ProximityDetectionSystem
 import io.github.quillraven.foxventure.system.RenderSystem
 import io.github.quillraven.foxventure.system.SpawnSystem
 import io.github.quillraven.foxventure.tiled.LoadTileObjectListener
@@ -59,6 +61,8 @@ class GameScreen(
             add(SpawnSystem())
             add(ActivationSystem())
             add(PreInterpolationSystem()) // run it before any physics system runs (climb, aerial, ground)
+            add(ProximityDetectionSystem())
+            add(FollowSystem())
             add(ClimbSystem())
             add(AerialMoveSystem())
             add(GroundMoveSystem())
