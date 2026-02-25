c16f495e44e82c66ed972d109462185756d7df41
Quillraven <quillraven@gmail.com>
2026-02-25 21:51:01 +0100

    add mushroom idle + run state

--- core/src/main/kotlin/io/github/quillraven/foxventure/ai/mushroomState.kt
+++ core/src/main/kotlin/io/github/quillraven/foxventure/ai/mushroomState.kt
@@ -4,6 +4,8 @@ import com.github.quillraven.fleks.Entity
 import com.github.quillraven.fleks.World
 import io.github.quillraven.foxventure.component.Animation
 import io.github.quillraven.foxventure.component.AnimationType
+import io.github.quillraven.foxventure.component.Fsm
+import io.github.quillraven.foxventure.component.Velocity
 
 data object MushroomStateIdle : FsmState {
     override fun World.onEnter(entity: Entity) {
@@ -11,6 +13,22 @@ data object MushroomStateIdle : FsmState {
     }
 
     override fun World.onUpdate(entity: Entity) {
-        // TODO
+        val velocity = entity[Velocity]
+        if (velocity.current.x != 0f) {
+            entity[Fsm].state.changeState(MushroomStateRun)
+        }
+    }
+}
+
+data object MushroomStateRun : FsmState {
+    override fun World.onEnter(entity: Entity) {
+        entity[Animation].changeTo(AnimationType.RUN)
+    }
+
+    override fun World.onUpdate(entity: Entity) {
+        val velocity = entity[Velocity]
+        if (velocity.current.x == 0f) {
+            entity[Fsm].state.changeState(MushroomStateIdle)
+        }
     }
 }
