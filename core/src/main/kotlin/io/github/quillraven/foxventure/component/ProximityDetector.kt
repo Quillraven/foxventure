14adc1acedef0072354ecd107a25303a437360cb
Quillraven <quillraven@gmail.com>
2026-02-25 21:50:37 +0100

    add proximity detector system

---
+++ core/src/main/kotlin/io/github/quillraven/foxventure/component/ProximityDetector.kt
@@ -0,0 +1,17 @@
+package io.github.quillraven.foxventure.component
+
+import com.github.quillraven.fleks.Component
+import com.github.quillraven.fleks.ComponentType
+import com.github.quillraven.fleks.Entity
+import com.github.quillraven.fleks.World
+
+data class ProximityDetector(
+    var range: Float,
+    var predicate: (World.(target: Entity) -> Boolean),
+    var onDetect: (World.(source: Entity, target: Entity) -> Unit),
+    var target: Entity = Entity.NONE,
+) : Component<ProximityDetector> {
+    override fun type() = ProximityDetector
+
+    companion object : ComponentType<ProximityDetector>()
+}
