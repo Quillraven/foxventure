5b81b63a2914e28a7f047b333c467dcc5bf74def
Quillraven <quillraven@gmail.com>
2026-02-27 16:40:22 +0100

    add functionality to damage enemies when jumping on them and damaging player when running into enemies

--- core/src/main/kotlin/io/github/quillraven/foxventure/system/CollisionSystem.kt
+++ core/src/main/kotlin/io/github/quillraven/foxventure/system/CollisionSystem.kt
@@ -10,13 +10,18 @@ import com.github.quillraven.fleks.collection.compareEntity
 import io.github.quillraven.foxventure.Asset.Companion.get
 import io.github.quillraven.foxventure.AtlasAsset
 import io.github.quillraven.foxventure.component.Collision
+import io.github.quillraven.foxventure.component.Controller
 import io.github.quillraven.foxventure.component.Damage
 import io.github.quillraven.foxventure.component.Damaged
 import io.github.quillraven.foxventure.component.EntityTag
 import io.github.quillraven.foxventure.component.GdxAnimation
+import io.github.quillraven.foxventure.component.Physics
 import io.github.quillraven.foxventure.component.Player
+import io.github.quillraven.foxventure.component.Rect
 import io.github.quillraven.foxventure.component.Transform
 import io.github.quillraven.foxventure.component.Type
+import io.github.quillraven.foxventure.component.Velocity
+import io.github.quillraven.foxventure.input.Command
 import io.github.quillraven.foxventure.system.RenderSystem.Companion.sfx
 
 class CollisionSystem(
@@ -42,7 +47,8 @@ class CollisionSystem(
     }
 
     override fun onTickEntity(entity: Entity) {
-        val (position) = entity[Transform]
+        val entityTransform = entity[Transform]
+        val (position) = entityTransform
         val collBox = entity[Collision].box
         val type = entity.getOrNull(Type)?.type ?: ""
 
@@ -53,14 +59,33 @@ class CollisionSystem(
                 return@forEach
             }
 
-            val (otherPosition) = other[Transform]
+            val otherTransform = other[Transform]
+            val (otherPosition) = otherTransform
             val otherCollBox = other[Collision].box
             val otherType = other.getOrNull(Type)?.type ?: ""
 
             if (collBox.overlaps(position, otherPosition, otherCollBox)) {
                 when {
-                    "player" == type -> onPlayerCollision(player = entity, other = other, otherType = otherType)
-                    "player" == otherType -> onPlayerCollision(player = other, other = entity, otherType = type)
+                    "player" == type -> onPlayerCollision(
+                        player = entity,
+                        entityTransform,
+                        collBox,
+                        other = other,
+                        otherTransform,
+                        otherCollBox,
+                        otherType = otherType
+                    )
+
+                    "player" == otherType -> onPlayerCollision(
+                        player = other,
+                        otherTransform,
+                        otherCollBox,
+                        other = entity,
+                        entityTransform,
+                        collBox,
+                        otherType = type
+                    )
+
                     else -> onCollision(entity = entity, other = other)
                 }
             }
@@ -70,7 +95,15 @@ class CollisionSystem(
     @Suppress("unused")
     private fun onCollision(entity: Entity, other: Entity) = Unit
 
-    private fun onPlayerCollision(player: Entity, other: Entity, otherType: String) {
+    private fun onPlayerCollision(
+        player: Entity,
+        playerTransform: Transform,
+        playerCollBox: Rect,
+        other: Entity,
+        otherTransform: Transform,
+        otherCollBox: Rect,
+        otherType: String
+    ) {
         when (otherType) {
             "gem" -> {
                 player[Player].gems++
@@ -82,14 +115,29 @@ class CollisionSystem(
                 audioService.playSound("pickup.wav")
             }
 
-            "damage" -> {
+            "damage" if (player hasNo Damaged) -> {
                 val (source, damageAmount) = other[Damage]
                 player.configure { it += Damaged(source, invulnerableTime = 1f, damageAmount) }
                 other.remove()
             }
 
             "enemy" -> {
-                // TODO: Handle player-enemy collision (damage, knockback, etc.)
+                val playerBottom = playerTransform.position.y + playerCollBox.y
+                val enemyTop = otherTransform.position.y + otherCollBox.y + otherCollBox.height
+                val tolerance = 0.25f
+                val playerVelocity = player[Velocity].current
+
+                if (playerBottom + tolerance > enemyTop && playerVelocity.y < 0f && other hasNo Damaged) {
+                    // player stomps on an enemy from above
+                    val jumpPressed = player[Controller].hasCommand(Command.JUMP)
+                    val physics = player[Physics]
+                    playerVelocity.y = if (jumpPressed) physics.jumpImpulse else physics.jumpImpulse * 0.7f
+
+                    other.configure { it += Damaged(player, invulnerableTime = 0.5f, damage = 1) }
+                } else if (player hasNo Damaged) {
+                    // the player collides from the side or below - the player gets damaged
+                    player.configure { it += Damaged(other, invulnerableTime = 1f, damage = 1) }
+                }
             }
         }
     }
