2bb860a0310945d186d200c046c7854fda2a4532
Quillraven <quillraven@gmail.com>
2026-03-12 10:09:49 +0100

    fix projectile spawn location

--- core/src/main/kotlin/io/github/quillraven/foxventure/system/ProjectileRequestSystem.kt
+++ core/src/main/kotlin/io/github/quillraven/foxventure/system/ProjectileRequestSystem.kt
@@ -29,7 +29,7 @@ class ProjectileRequestSystem(
     private val objectsAtlas = assets[AtlasAsset.OBJECTS]
 
     override fun onTickEntity(entity: Entity) {
-        val (source, target, damage, spawnOffset, atlasKey, size, collisionRect, speed, playMode) = entity[ProjectileRequest]
+        val (source, target, damage, spawnOffset, atlasKey, projectileSize, collisionRect, speed, playMode) = entity[ProjectileRequest]
         if (source.wasRemoved() || target.wasRemoved()) {
             entity.remove()
             return
@@ -39,20 +39,23 @@ class ProjectileRequestSystem(
         val targetCollBox = target[Collision].box
         val targetCenterX = targetTransform.position.x + targetCollBox.x + targetCollBox.width * 0.5f
 
-        val sourceTransform = source[Transform]
+        val (sourcePosition, sourceSize, _, _, sourceZ) = source[Transform]
         val sourceCollBox = source[Collision].box
-        val sourceX = sourceTransform.position.x + sourceCollBox.x
-        val sourceCenterX = sourceX + sourceCollBox.width * 0.5f
+        val sourceX = sourcePosition.x
+        val sourceCenterX = sourceX + sourceCollBox.x + sourceCollBox.width * 0.5f
 
         val targetIsLeft = targetCenterX < sourceCenterX
-        val projectileX = if (targetIsLeft) sourceX + sourceCollBox.width - spawnOffset.x else sourceX + spawnOffset.x
-        val projectileY = sourceTransform.position.y + sourceCollBox.y + spawnOffset.y
+        val projectileX = when {
+            targetIsLeft -> sourceX + sourceSize.x - spawnOffset.x - projectileSize.x
+            else -> sourceX + spawnOffset.x
+        }
+        val projectileY = sourcePosition.y + spawnOffset.y - projectileSize.y * 0.5f
         val projectilePosition = vec2(projectileX, projectileY)
 
         world.entity {
             val gdxAnimation = objectsAtlas.getGdxAnimation(atlasKey, AnimationType.IDLE, playMode)
 
-            it += Transform(projectilePosition, size.cpy(), z = sourceTransform.z - 1)
+            it += Transform(projectilePosition, projectileSize.cpy(), z = sourceZ - 1)
             it += Graphic(gdxAnimation.getKeyFrame(0f), flip = targetIsLeft)
             it += Animation(objectKey = atlasKey, gdxAnimation, gdxAnimations = emptyMap(), speed = 1f)
             it += Velocity().apply { current.x = if (targetIsLeft) -speed else speed }
