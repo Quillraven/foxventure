9a7d6aabefaa970ca77b160cfc85fc2e6059195a
Quillraven <quillraven@gmail.com>
2026-03-23 14:47:29 +0000

    Deploying to gh-pages from @ Quillraven/foxventure@b5dac88d4dfc8a1eb949db1afb3b47de9cf3e413 🚀

--- src/io/github/quillraven/foxventure/input/Command.kt
+++
@@ -1,9 +0,0 @@
-package io.github.quillraven.foxventure.input
-
-enum class Command {
-    MOVE_LEFT,
-    MOVE_RIGHT,
-    MOVE_UP,
-    MOVE_DOWN,
-    JUMP
-}
--- src/io/github/quillraven/foxventure/ui/notifyDelegate.kt
+++
@@ -1,7 +0,0 @@
-package io.github.quillraven.foxventure.ui
-
-import kotlin.properties.Delegates
-import kotlin.properties.ReadWriteProperty
-
-fun <T> notifying(initial: T, onChange: (T) -> Unit): ReadWriteProperty<Any?, T> =
-    Delegates.observable(initial) { _, old, new -> if (old != new) onChange(new) }
