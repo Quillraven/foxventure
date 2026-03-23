9856e868c87fcc8274ee225301b77bcc2f110b21
Quillraven <quillraven@gmail.com>
2026-03-23 06:56:06 +0100

    add title and value labels to slider

--- core/src/main/kotlin/io/github/quillraven/foxventure/ui/MainMenuView.kt
+++ core/src/main/kotlin/io/github/quillraven/foxventure/ui/MainMenuView.kt
@@ -1,6 +1,7 @@
 package io.github.quillraven.foxventure.ui
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.ui.Label
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Table
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton
@@ -22,7 +23,7 @@ class MainMenuView(
             pad(16f)
             defaults().padBottom(12f).fillX().minWidth(200f)
             isTransform = true
-            setScale(0.85f)
+            setScale(0.75f)
         }
 
         val startButton = TextButton("Start Game", skin)
@@ -31,13 +32,8 @@ class MainMenuView(
         })
         content.add(startButton).row()
 
-        val musicSlider = VolumeSlider(skin, viewModel.musicVolume)
-        musicSlider.onValueChanged = { viewModel.musicVolume = it }
-        content.add(musicSlider).row()
-
-        val soundSlider = VolumeSlider(skin, viewModel.soundVolume)
-        soundSlider.onValueChanged = { viewModel.soundVolume = it }
-        content.add(soundSlider).row()
+        content.add(sliderBlock("Music Volume", skin, viewModel.musicVolume) { viewModel.musicVolume = it }).row()
+        content.add(sliderBlock("Sound Volume", skin, viewModel.soundVolume) { viewModel.soundVolume = it }).row()
 
         if (!isWeb) {
             val quitButton = TextButton("Quit Game", skin)
@@ -48,5 +44,24 @@ class MainMenuView(
         }
 
         add(content).center().padBottom(20f)
+    }
+
+    private fun sliderBlock(title: String, skin: Skin, initialValue: Float, onChange: (Float) -> Unit): Table {
+        val block = Table(skin)
+
+        block.add(Label(title, skin, "border")).left().padBottom(5f).row()
+
+        val slider = VolumeSlider(skin, initialValue)
+        slider.onValueChanged = onChange
+        block.add(slider).fillX().row()
+
+        val minLabel = Label("0", skin, "small_border")
+        val maxLabel = Label("100", skin, "small_border")
+        block.add(Table(skin).also { row ->
+            row.add(minLabel).left().expandX().padTop(5f).padLeft(2f)
+            row.add(maxLabel).right().padTop(5f).padLeft(20f)
+        }).fillX().row()
+
+        return block
     }
 }
